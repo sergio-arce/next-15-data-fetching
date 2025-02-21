76382eaec164b59ffab309199256c5d7a26aae90
Sergio Arce <arcesech30@gmail.com>
2025-02-21 11:50:53 +0100

    add fetching from database use prisma in products-db page

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 
 [JSON Placehoder link](jsonplaceholder.typicode.com)
+
+
+Prisma
+
+- pnpm add prisma -D
+- npx prisma init --datasource-provider sqlite
+- npx prisma migrate dev --name init
---
+++ src/app/products-db/page.tsx
@@ -0,0 +1,30 @@
+import Link from "next/link";
+import { getProducts } from "../prisma-db";
+
+export type Product = {
+  id: number;
+  title: string;
+  price: number;
+  description: string | null;
+};
+
+export default async function ProductsDBPage() {
+
+  const products: Product[] = await getProducts()
+  return (
+    <ul className="space-y-4 p-4">
+      {products.map((product) => (
+        <li
+          key={product.id}
+          className="p-4 bg-white shadow-md rounded-lg text-gray-700"
+        >
+          <h2 className="text-xl font-semibold">
+            <Link href={`/products-db/${product.id}`}>{product.title}</Link>
+          </h2>
+          <p>{product.description}</p>
+          <p className="text-lg font-medium">${product.price}</p>
+        </li>
+      ))}
+    </ul>
+  );
+}
