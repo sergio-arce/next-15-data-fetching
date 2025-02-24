0d173731bb4597ffabd700924dad503996c1d9f0
Sergio Arce <arcesech30@gmail.com>
2025-02-24 18:27:41 +0100

    add server action update data in product-db page

---
+++ src/app/products-db/[id]/page.tsx
@@ -0,0 +1,17 @@
+import { getProduct } from "@/app/prisma-db";
+import EditProductForm from "./product-edit-form";
+import { notFound } from "next/navigation";
+import { Product } from '@/app/products-db/page';
+
+export default async function EditProductPage({ params }: { params: Promise<{ id: string }>}) {
+
+  const { id } = await params
+
+  const product: Product | null = await getProduct(parseInt(id))
+
+  if (!product) {
+    notFound()
+  }
+
+  return <EditProductForm product={product} />
+}
---
+++ src/app/products-db/[id]/product-edit-form.tsx
@@ -0,0 +1,65 @@
+"use client";
+
+import { FormState, editProduct } from "@/actions/products";
+import { Submit } from "@/components/submit";
+import { useActionState } from "react";
+import { Product } from '@/app/products-db/page';
+
+export default function EditProductForm({ product }: { product: Product }) {
+
+  const initialState: FormState = {
+    errors: {},
+  };
+
+  const editProductWithId = editProduct.bind(null, product.id)
+
+  const [state, formAction] = useActionState(editProductWithId, initialState);
+
+  return (
+    <form action={formAction} className="p-4 space-y-4 max-w-96">
+      <input type="hidden" name="id" value={product.id} />
+      <div>
+        <label className="text-white">
+          Title
+          <input
+            type="text"
+            className="block w-full p-2 text-black border rounded"
+            name="title"
+            defaultValue={product.title}
+          />
+        </label>
+        {state.errors.title && (
+          <p className="text-red-500">{state.errors.title}</p>
+        )}
+      </div>
+      <div>
+        <label className="text-white">
+          Price
+          <input
+            type="number"
+            className="block w-full p-2 text-black border rounded"
+            name="price"
+            defaultValue={product.price}
+          />
+        </label>
+        {state.errors.price && (
+          <p className="text-red-500">{state.errors.price}</p>
+        )}
+      </div>
+      <div>
+        <label className="text-white">
+          Description
+          <textarea
+            className="block w-full p-2 text-black border rounded"
+            name="description"
+            defaultValue={product.description ?? ""}
+          />
+        </label>
+        {state.errors.description && (
+          <p className="text-red-500">{state.errors.description}</p>
+        )}
+      </div>
+      <Submit />
+    </form>
+  );
+}
