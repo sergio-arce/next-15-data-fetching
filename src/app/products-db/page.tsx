03f16b7227d679d80e5ca385d4b5bce601bca885
Sergio Arce <arcesech30@gmail.com>
2025-02-25 15:47:40 +0100

    add optimistic updates in products-db and products-detail page

--- src/app/products-db/page.tsx
+++ src/app/products-db/page.tsx
@@ -1,6 +1,5 @@
-import Link from "next/link";
-import { getProducts } from "../prisma-db";
-import { removeProduct } from "@/actions/products";
+import { getProducts } from "@/app/prisma-db";
+import { ProductDetail } from "./product-detail";
 
 export type Product = {
   id: number;
@@ -9,33 +8,13 @@ export type Product = {
   description: string | null;
 };
 
-export default async function ProductsDBPage() {
+export default async function ProductsPrismaDBPage({
+  searchParams,
+}: {
+  searchParams: Promise<{ query?: string }>;
+}) {
+  const { query } = await searchParams;
+  const products: Product[] = await getProducts(query);
 
-  const products: Product[] = await getProducts()
-  return (
-    <ul className="space-y-4 p-4">
-      {products.map((product) => (
-        <li
-          key={product.id}
-          className="p-4 bg-white shadow-md rounded-lg text-gray-700"
-        >
-          <h2 className="text-xl font-semibold">
-            <Link href={`/products-db/${product.id}`}>{product.title}</Link>
-          </h2>
-          <p>{product.description}</p>
-          <p className="text-lg font-medium">${product.price}</p>
-          {/* <form action={removeProduct.bind(null, product.id)}> */}
-          <form action={removeProduct}>
-            <input type="hidden" name="productId" value={product.id}/>
-            <button
-              type="submit"
-              className="px-4 py-2 mt-4 text-white bg-red-500 rounded-md hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-red-600"
-            >
-              Delete
-            </button>
-          </form>
-        </li>
-      ))}
-    </ul>
-  );
+  return <ProductDetail products={products} />;
 }
